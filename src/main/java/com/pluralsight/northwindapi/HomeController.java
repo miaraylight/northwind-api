15265e25fed8d94cb9a04d56b8a0cd232308c82b
miaraylight <miaraylight@gmail.com>
2025-12-11 09:09:19 -0600

    add without params

--- src/main/java/com/pluralsight/northwindapi/HomeController.java
+++ src/main/java/com/pluralsight/northwindapi/HomeController.java
@@ -1,12 +1,13 @@
 package com.pluralsight.northwindapi;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @RequestMapping(path = "/")
-    public String index(){
-        return "Hello world";
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String index(String country){
+        return "Hello " + country;
     }
 }
