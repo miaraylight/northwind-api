df989a2a08ad3b2b55183bc7b4f266b10ffb6c83
miaraylight <miaraylight@gmail.com>
2025-12-11 15:14:59 -0600

    add Product Controller

--- src/main/java/com/pluralsight/northwindapi/controller/ProductController.java
+++ src/main/java/com/pluralsight/northwindapi/controller/ProductController.java
@@ -1,4 +1,26 @@
 package com.pluralsight.northwindapi.controller;
 
+import com.pluralsight.northwindapi.dao.ProductDao;
+import com.pluralsight.northwindapi.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+    @Autowired
+    ProductDao productDao;
+
+    @RequestMapping(path = "/products")
+    public List<Product> getAll() {
+        return productDao.getAll();
+    }
+
+    @RequestMapping(path = "/products/{id}")
+    public Product getById(@PathVariable int id) {
+        return productDao.getById(id);
+    }
 }
