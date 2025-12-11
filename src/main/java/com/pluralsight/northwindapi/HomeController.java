4a74d4021e52029007f549709b8a06ea921de1af
miaraylight <miaraylight@gmail.com>
2025-12-11 08:54:29 -0600

    first commit

---
+++ src/main/java/com/pluralsight/northwindapi/HomeController.java
@@ -0,0 +1,12 @@
+package com.pluralsight.northwindapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(path = "/")
+    public String index(){
+        return "Hello world";
+    }
+}
---
+++ src/main/java/com/pluralsight/northwindapi/NorthwindTraderApiApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwindapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindTraderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindTraderApiApplication.class, args);
+	}
+
+}
